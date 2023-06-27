956ea6eb7f7c160239fe02fcabe8faced3f831a6
Tomer_Kulish <91198141+tomerkul@users.noreply.github.com>
2023-06-27 15:01:31 +0300

    Update pulllatest.py

--- sample-flask/pulllatest.py
+++ sample-flask/pulllatest.py
@@ -3,16 +3,32 @@ import docker
 # Create a Docker client
 client = docker.from_env()
 
-# Define the image name
-image_name = "tomerkul/myflask"
+# Image details
+repo_name = 'tomerkul/myflask'
 
-# List all available tags for the image on Docker Hub
-tags = client.images.list(name=image_name)
+# Get the available images
+images = client.images.list()
 
-# Sort the tags in descending order and get the latest version
-latest_tag = sorted(tags, key=lambda x: x.attrs['Created'], reverse=True)[0].tags[0]
+# Get the available image tags
+tags = []
+for image in images:
+    for tag in image.tags:
+        if repo_name in tag:
+            tags.append(tag.split(':')[1])
 
-# Pull the latest version of the image
-client.images.pull(repository=image_name, tag=latest_tag)
+# Filter out any non-numeric tags
+numeric_tags = [tag for tag in tags if tag.replace('.', '').isdigit()]
 
-print(f"Successfully pulled {image_name}:{latest_tag}")
+# Sort the numeric tags in descending order
+sorted_tags = sorted(numeric_tags, key=lambda x: tuple(map(int, x.split('.'))), reverse=True)
+
+if sorted_tags:
+    # Get the latest tag
+    latest_tag = sorted_tags[0]
+
+    # Pull the latest image
+    image_name = f"{repo_name}:{latest_tag}"
+    print(f"Pulling image: {image_name}")
+    client.images.pull(repository=repo_name, tag=latest_tag)
+else:
+    print("No image found with version tags.")
